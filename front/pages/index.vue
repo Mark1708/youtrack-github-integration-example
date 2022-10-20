1c7eab6ec90dd575b1abcc0466d01e6068735e98
Mark1708 <mark1708.work@gmail.com>
2022-10-20 11:56:41 +0300

    Init front #DD-24 Done

---
+++ front/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      title: 'Home'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'Home page',
+          name: 'Home page',
+          content: 'My web site',
+        }
+      ]
+    }
+  }
+})
+</script>
